462ff9d40639f9c7a641a1dbc9425584b82ba457
TszyaoEkaterina <3022805283@qq.com>
2022-07-25 22:28:11 +0800

    calc average method added

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,29 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int min() {
+        int min = 1_999_999_999;
+        for (int sale : sales) {
+            if (sale < min) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int avg() {
+        int max = max();
+        int min = min();
+        int i = 0;
+        int sum = 0;
+        for (int sale : sales) {
+            if (sale != max && sale != min) {
+                i++;
+                sum += sale;
+            }
+        }
+        int avg = sum / i;
+        return avg;
+    }
 }
